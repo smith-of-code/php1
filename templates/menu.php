c79ced2d4a850d7a5015cdf31f579445969fe44e
Andrey Kleynos <goodmena@yandex.ru>
2019-10-08 14:12:02 +0300

    lesson3

--- templates/menu.php
+++ templates/menu.php
@@ -1,3 +1,17 @@
-<a href="/">Главная</a>
-<a href="/catalog.php">Каталог</a>
-<br>
+<?
+function getMenuItem($menu){
+    $result = "";
+    foreach ($menu as $title => $link){
+        if (is_array($link)){
+            $result .= "<li class='menu__item drop__item'>$title<ul class='sub__menu'>". getMenuItem($link) ."</ul></li>";
+        }else{
+            $result .= "<li class='menu__item'><a href='$link'>$title</a></li>";
+        }
+    }
+    return $result;
+}
+?>
+<ul class="menu">
+ <?=getMenuItem($nav)?>
+</ul>
+
