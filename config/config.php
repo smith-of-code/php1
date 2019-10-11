288443a482aa998b502d20f4ec1114cb325bf074
Andrey Kleynos <goodmena@yandex.ru>
2019-10-11 15:46:28 +0300

    lesson4 и еще одно поправил))

--- config/config.php
+++ config/config.php
@@ -4,6 +4,6 @@ define('TEMPLATES_DIR', DIR_ROOT.'/templates/');
 define('LAYOUTS_DIR', '/layouts/');
 define('GALLERY_DIR', './img');
 
-require_once "../engine/functions.php";
+require_once DIR_ROOT . "/engine/functions.php";
 
 
