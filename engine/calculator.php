95691bfecdde29980d5ea72e70154d14b79abe34
Andrey Kleynos <goodmena@yandex.ru>
2019-10-19 03:50:29 +0300

    lesson6 подправил деление на 0

--- engine/calculator.php
+++ engine/calculator.php
@@ -33,7 +33,7 @@ function mathOperation(){
             $result[] = $a - $b;
             break;
         case ('/'):
-            if($b !== 0){
+            if($b === 0){
                 $result[] = $a / $b;
             }else{
                 $result[] = 'На ноль делить нельзя';
